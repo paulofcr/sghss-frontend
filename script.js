01301e90e7baa8cb27dae9667f24f5bc9cb6c3c6
paulofcr <paulofcr@hotmail.com>
2026-05-17 13:48:04 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
+function trocarTela(nome){
+  document.getElementById("titulo").innerText = nome;
 
+  document.getElementById("cards").innerHTML = `
+    <div>${nome}<br><strong>ativo</strong></div>
+    <div>Status<br><strong>Online</strong></div>
+    <div>Atualizado<br><strong>Agora</strong></div>
+  `;
+}
+
+function toggleMenu(){
+  document.getElementById("sidebar").classList.toggle("active");
+}
